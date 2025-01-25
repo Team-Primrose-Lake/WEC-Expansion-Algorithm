738472c065c0a4753557140e81044540feafd1d0
Marcus Fu <38443839+Ecodelis@users.noreply.github.com>
2025-01-25 10:02:35 -0600

    added fire_hall_algorithm.m file

---
+++ fire_hall_algorithm.m
@@ -0,0 +1,6 @@
+% File: fire_hall_algorithm.m
+% Description: An algorithm to calculate efficient placement of fire halls
+% that span a 2.5km radius
+% Author: Marcus Fu, Taewoo Kim
+% Date: 2025-01-25
+
